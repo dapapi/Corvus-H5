375bdd3a463a638e4fdb863cdf6fa9a802e35dfb
Apple-0722 <514910332@qq.com>
2019-01-17 18:33:09 +0800

    qsf

--- src/views/artist/add.vue
+++ src/views/artist/add.vue
@@ -5,8 +5,15 @@
             <!--性别-->
             <Cell title="性别" is-link @click.native="changeState('popupGender',!popupGender)" :value="gender.name"></Cell>
             <Selector :visible="popupGender" :data="genderArr" @change="changeGender" />
-
-            <Cell title="出生日期" is-link></Cell>
+           <!--出生日期-->
+            <Cell title="出生日期" is-link @click.native="changeBornTime" :value="bornTime"></Cell>
+            <DatetimePicker
+                ref="bornPicker"
+                type="date"
+                v-model="defaultDate"
+                :startDate="startDate"
+                @confirm="bornConfirm"
+            />
             <!--艺人来源-->
             <Cell title="艺人来源" is-link @click.native="changeState('popupArtistSource',!popupArtistSource)" :value="artistSource.name"></Cell>
             <Selector :visible="popupArtistSource" :data="artistSourceArr" @change="changeArtistSource" />
@@ -17,16 +24,16 @@
             <Field label="星探姓名" v-model="scout"></Field>
             <Field label="地区" v-model="region"></Field>
             <!--平台-->
-            <Cell title="平台" is-link @click.native="checkResource()"><span class="mint-cell-mask"></span></Cell>
+            <Cell title="平台" is-link @click.native="checkResource()" :value="platformName"></Cell>
             
-            <Field placeholder="微博主页地址"  v-model="email"></Field>
-            <Field placeholder="微博粉丝数" v-model="phone"></Field>
-            <Field placeholder="百科地址" v-model="wechat"></Field>
-            <Field placeholder="百科粉丝数" v-model="scout"></Field>
-            <Field placeholder="抖音ID" v-model="region"></Field>
-            <Field placeholder="抖音粉丝数" v-model="email"></Field>
-            <Field placeholder="其他地址" v-model="phone"></Field>
-            <Field placeholder="其他地址粉丝数" v-model="wechat"></Field>
+            <Field class="text-left" placeholder="微博主页地址" v-if="selectedPlatform.find(item => item ==1)" v-model="weiboUrl"></Field>
+            <Field class="text-left" placeholder="微博粉丝数" v-if="selectedPlatform.find(item => item ==1)" v-model="weiboFansNum"></Field>
+            <Field class="text-left" placeholder="百科地址" v-if="selectedPlatform.find(item => item ==2)" v-model="baikeUrl"></Field>
+            <Field class="text-left" placeholder="百科粉丝数" v-if="selectedPlatform.find(item => item ==2)" v-model="baikeFansNum"></Field>
+            <Field class="text-left" placeholder="抖音ID" v-if="selectedPlatform.find(item => item ==3)" v-model="douyinId"></Field>
+            <Field class="text-left" placeholder="抖音粉丝数" v-if="selectedPlatform.find(item => item ==3)" v-model="douyinFansNum"></Field>
+            <Field class="text-left" placeholder="其他地址" v-if="selectedPlatform.find(item => item ==4)" v-model="qitaUrl"></Field>
+            <Field class="text-left" placeholder="其他地址粉丝数" v-if="selectedPlatform.find(item => item ==4)" v-model="qitaFansNum"></Field>
             
             <!--沟通状态-->
             <Cell title="沟通状态" is-link  @click.native="changeState('popupArtistStatus',!popupArtistStatus)" :value="artistStatus.name"></Cell>
@@ -48,21 +55,22 @@
             <Field label="备注" v-model="remark"></Field>
         </div>
         <CheckList v-show='popupPlatform' :selectorData="artistPlatformList" :multiple="true" @change="seletedData"/>
-       
-            
-        
     </div>
 </template>
 <script>
 import config from '@/utils/config.js'
+import moment from 'moment'
 export default {
     data(){
         return {
             username:'',//姓名
-            popupGender:false,
+            popupGender:false,//性别
             genderArr:config.genderArr,
             gender:{},
-           
+            bornTime:'',//出生日期
+            startDate:null,
+            defaultDate:new Date(),
+
             artistSourceArr:config.artistSourceArr,//艺人来源
             popupArtistSource:false,
             artistSource:{},
@@ -76,8 +84,15 @@ export default {
             artistPlatformList:config.artistPlatformList,//平台列表
             popupPlatform:false,
             selectedPlatform:[],//选中的平台
-
-
+            platformName:'',
+            weiboUrl: '',
+            weiboFansNum: '',
+            douyinId: '',
+            douyinFansNum: '',
+            baikeUrl: '',
+            baikeFansNum: '',
+            qitaUrl: '',
+            qitaFansNum: '',
 
             artistStatusArr:config.artistStatusArr,//沟通状态
             popupArtistStatus:false,
@@ -92,6 +107,9 @@ export default {
             remark:'',//备注
         }
     },
+    created(){
+        this.startDate = new Date('1900')
+    },
     methods:{
         
         changeState (name, value) {
@@ -101,6 +119,16 @@ export default {
         changeGender:function(data){
            this.popupGender = !this.popupGender
            this.gender = data
+        },
+        //出生日期
+        changeBornTime:function(){
+            if(!this.bornTime){
+               this.bornTime = moment(this.defaultDate).format('YYYY-MM-DD')
+            }
+           this.$refs.bornPicker.open()
+        },
+        bornConfirm (date) {
+            this.bornTime = moment(date).format('YYYY-MM-DD')
         },
         //艺人来源
         changeArtistSource:function(data){
@@ -128,8 +156,16 @@ export default {
            if(isHidden){
                this.popupPlatform = false
            }
+           let platformName =[]
            this.selectedPlatform = data
-           console.log(this.selectedPlatform)
+           for (let i = 0; i < this.artistPlatformList.length; i++) {
+               if(this.selectedPlatform.find(item => item ==this.artistPlatformList[i].value)){
+                   platformName.push(this.artistPlatformList[i].label)
+               }
+               
+           }
+           this.platformName = platformName.join(',')
+           console.log(this.platformName)
            
         },
         checkResource:function(){
