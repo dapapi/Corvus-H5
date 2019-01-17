f9ee22a61a6368fae680c135ba0bb4b585e673d6
sufang <sufang0722@163.com>
2019-01-17 17:01:46 +0800

    qsf

--- src/utils/config.js
+++ src/utils/config.js
@@ -18,5 +18,125 @@ export default {
       name: 'C',
       value: 0
     }
-  ]
+  ],
+  // 性别
+  genderArr: [{
+        name: '男',
+        value: 1
+      },
+      {
+        name: '女',
+        value: 2
+      }
+    ],
+    //艺人来源
+    artistSourceArr: [
+
+      {
+        name: '线上',
+        value: 1
+      },
+      {
+        name: '线下',
+        value: 2
+      },
+      {
+        name: '抖音',
+        value: 3
+      },
+      {
+        name: '微博',
+        value: 4
+      },
+      {
+        name: '陈赫',
+        value: 5
+      },
+      {
+        name: '北电',
+        value: 6
+      },
+      {
+        name: '杨光',
+        value: 7
+      },
+      {
+        name: '中戏',
+        value: 8
+      },
+      {
+        name: 'papitube推荐',
+        value: 9
+      },
+      {
+        name: '地标商圈',
+        value: 10
+      },
+    ],
+    // 艺人沟通状态
+    artistStatusArr: [
+
+      {
+        name: '已签约',
+        value: 1,
+        color: '#4CAF50'
+      },
+      {
+        name: '经理人沟通中',
+        value: 2,
+        color: "#00BCD4"
+      },
+      {
+        name: '兼职星探沟通中',
+        value: 3,
+        color: "#00B0FF"
+      },
+      {
+        name: '待定',
+        value: 4,
+        color: '#FF9800'
+      },
+      {
+        name: '淘汰',
+        value: 5,
+        color: '#9e9e9e'
+      },
+      {
+        name: '合同中',
+        value: 6,
+        color: '#9c27b0'
+      },
+      {
+        name: '联系但无回复',
+        value: 7,
+        color: '#f44336'
+      },
+    ],
+    yesOrNo: [{
+        name: '是',
+        value: 1,
+      },
+      {
+        name: '否',
+        value: 2
+      }
+    ],
+    artistPlatformList: [{
+        value: 1,
+        label: '微博'
+      },
+      {
+        value: 2,
+        label: '百科'
+      },
+      {
+        value: 3,
+        label: '抖音'
+      },
+      {
+        value: 4,
+        label: '其他'
+      },
+    ],
 }
+
--- src/views/artist/add.vue
+++ src/views/artist/add.vue
@@ -1,43 +1,56 @@
 <template>
     <div>
-        <Field label="姓名" v-model="username"></Field>
-        <Cell title="性别" is-link @click.native="changeVisible"></Cell>
-        <Popup position="bottom" v-model="popupVisible" popup-transition="popup-fade" style="width: 100%">
-            <Cell title="男"><span class="mint-cell-mask"></span></Cell>
-            <Cell title="女"><span class="mint-cell-mask"></span></Cell>
-        </Popup>
-        <Cell title="出生日期" is-link></Cell>
-        <Cell title="艺人来源" is-link @click.native="changeArtistSource"></Cell>
-        <Popup position="bottom" v-model="artistSource" popup-transition="popup-fade" style="width: 100%">
-            <Cell v-for="(item,index) in artistSourceArr" :key="index" :title="item.name"><span class="mint-cell-mask" ></span></Cell>
-        </Popup>
-        <Field label="邮箱" v-model="email"></Field>
-        <Field label="手机号" v-model="phone"></Field>
-        <Field label="微信" v-model="wechat"></Field>
-        <Field label="星探姓名" v-model="scout"></Field>
-        <Field label="地区" v-model="region"></Field>
-        <Cell title="平台" is-link to="/selector"><span class="mint-cell-mask"></span></Cell>
-        <Cell title="沟通状态" is-link @click.native="changeArtistStatus"></Cell>
-        <Popup position="bottom" v-model="artistStatus" popup-transition="popup-fade" style="width: 100%">
-            <Cell v-for="(item,index) in artistStatusArr" :key="index" :title="item.name"><span class="mint-cell-mask" ></span></Cell>
-        </Popup>
+        <div v-show="!popupPlatform">
+            <Field label="姓名" v-model="username"></Field>
+            <!--性别-->
+            <Cell title="性别" is-link @click.native="changeState('popupGender',!popupGender)" :value="gender.name"></Cell>
+            <Selector :visible="popupGender" :data="genderArr" @change="changeGender" />
+
+            <Cell title="出生日期" is-link></Cell>
+            <!--艺人来源-->
+            <Cell title="艺人来源" is-link @click.native="changeState('popupArtistSource',!popupArtistSource)" :value="artistSource.name"></Cell>
+            <Selector :visible="popupArtistSource" :data="artistSourceArr" @change="changeArtistSource" />
+            
+            <Field label="邮箱" v-model="email"></Field>
+            <Field label="手机号" v-model="phone"></Field>
+            <Field label="微信" v-model="wechat"></Field>
+            <Field label="星探姓名" v-model="scout"></Field>
+            <Field label="地区" v-model="region"></Field>
+            <!--平台-->
+            <Cell title="平台" is-link @click.native="checkResource()"><span class="mint-cell-mask"></span></Cell>
+            
+            <Field placeholder="微博主页地址"  v-model="email"></Field>
+            <Field placeholder="微博粉丝数" v-model="phone"></Field>
+            <Field placeholder="百科地址" v-model="wechat"></Field>
+            <Field placeholder="百科粉丝数" v-model="scout"></Field>
+            <Field placeholder="抖音ID" v-model="region"></Field>
+            <Field placeholder="抖音粉丝数" v-model="email"></Field>
+            <Field placeholder="其他地址" v-model="phone"></Field>
+            <Field placeholder="其他地址粉丝数" v-model="wechat"></Field>
+            
+            <!--沟通状态-->
+            <Cell title="沟通状态" is-link  @click.native="changeState('popupArtistStatus',!popupArtistStatus)" :value="artistStatus.name"></Cell>
+            <Selector :visible="popupArtistStatus" :data="artistStatusArr" @change="changeArtistStatus" />
+            <!--与我司签约意向-->
+            <Cell title="与我司签约意向" is-link @click.native="changeState('popupIntention',!popupIntention)" :value="intention.name"></Cell>
+            <Selector :visible="popupIntention" :data="yesOrNo" @change="changeIntention" />
+            <Field class="text-left" v-show="intention.value == 2" v-model="intentionTxt" placeholder="请填写不签约理由"></Field>
+            <!--是否签约其他公司-->
+            <Cell title="是否签约其他公司" is-link  @click.native="changeState('popupSign',!popupSign)" :value="sign.name"></Cell>
+            <Selector :visible="popupSign" :data="yesOrNo" @change="changeSign" />
+            <Field class="text-left" v-show="sign.value == 1" v-model="company" placeholder="请输入已签约公司名称"></Field>
+
+            <Cell title="艺人头像">
+                <template>
+                    <FileUpload />
+                </template>
+            </Cell>
+            <Field label="备注" v-model="remark"></Field>
+        </div>
+        <CheckList v-show='popupPlatform' :selectorData="artistPlatformList" :multiple="true" @change="seletedData"/>
+       
+            
         
-        <Cell title="与我司签约意向" is-link @click.native="changeIsInput"></Cell>
-        <Popup position="bottom" v-model="isInput" popup-transition="popup-fade" style="width: 100%">
-            <Cell v-for="(item,index) in yesOrNo" :key="index" :title="item.name"><span class="mint-cell-mask" ></span></Cell>
-        </Popup>
-        <Field label="" v-model="username" placeholder="请填写不签约理由"></Field>
-        <Cell title="是否签约其他公司" is-link @click.native="changeIsInput"></Cell>
-        <Popup position="bottom" v-model="isInput" popup-transition="popup-fade" style="width: 100%">
-            <Cell v-for="(item,index) in yesOrNo" :key="index" :title="item.name"><span class="mint-cell-mask" ></span></Cell>
-        </Popup>
-        <Field label="公司名称" v-model="username" placeholder="请输入已签约公司名称"></Field>
-        <Cell title="艺人头像">
-            <template>
-                <FileUpload />
-            </template>
-        </Cell>
-        <Field label="备注" v-model="remark"></Field>
     </div>
 </template>
 <script>
@@ -46,39 +59,86 @@ export default {
     data(){
         return {
             username:'',//姓名
-            gender:false,
+            popupGender:false,
+            genderArr:config.genderArr,
+            gender:{},
+           
+            artistSourceArr:config.artistSourceArr,//艺人来源
+            popupArtistSource:false,
+            artistSource:{},
+
             email:'',//邮箱
             phone:'',//手机
             wechat:'',//微信
             scout:'',//星探
             region:'',//地区
-            genderArr:config.genderArr,
-            popupVisible:false,
-            artistSourceArr:config.artistSourceArr,
-            artistSource:false,
-            artistStatusArr:config.artistStatusArr,
-            artistStatus:false,
+
+            artistPlatformList:config.artistPlatformList,//平台列表
+            popupPlatform:false,
+            selectedPlatform:[],//选中的平台
+
+
+
+            artistStatusArr:config.artistStatusArr,//沟通状态
+            popupArtistStatus:false,
+            artistStatus:{},
             yesOrNo:config.yesOrNo,
-            isInput:false,
+            popupIntention:false,//签约意向
+            intention:{},
+            intentionTxt:'',//不签约 ---原因
+            popupSign:false,//是否与其他公司签约
+            sign:{},
+            company:'',//签约公司名称
             remark:'',//备注
         }
     },
     methods:{
-        changeGender:function(){
-           this.gender = true
+        
+        changeState (name, value) {
+            this[name] = value
         },
-        changeVisible:function(){
-            this.popupVisible = !this.popupVisible
+        //性别
+        changeGender:function(data){
+           this.popupGender = !this.popupGender
+           this.gender = data
         },
-        changeArtistSource:function(){
-            // alert(111)
-            this.artistSource = !this.artistSource
+        //艺人来源
+        changeArtistSource:function(data){
+            
+            this.popupArtistSource = !this.popupArtistSource
+            this.artistSource = data
         },
-        changeArtistStatus:function(){
-            this.artistStatus = !this.artistStatus
+        //沟通状态
+        changeArtistStatus:function(data){
+            this.popupArtistStatus = !this.popupArtistStatus
+            this.artistStatus = data
         },
-        changeIsInput:function(){
-            this.isInput = !this.isInput
+        //签约意向
+        changeIntention:function(data){
+            this.popupIntention = !this.popupIntention
+            this.intention = data
+        },
+        //签约公司
+        changeSign:function(data){
+            this.popupSign = !this.popupSign
+            this.sign = data
+        },
+        //平台
+        seletedData:function(data,isHidden){
+           if(isHidden){
+               this.popupPlatform = false
+           }
+           this.selectedPlatform = data
+           console.log(this.selectedPlatform)
+           
+        },
+        checkResource:function(){
+           this.popupPlatform = !this.popupPlatform
+           
+        },
+        //添加艺人
+        addArtist:function(){
+          
         }
     }
 }
@@ -89,4 +149,10 @@ export default {
   padding: .2rem;
   background-color: #fff;
 }
+.text-left{
+    input{
+        text-align:left!important;
+    }
+}
+
 </style>
