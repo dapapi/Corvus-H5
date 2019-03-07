8a5d525409fca904ce1becc0722d0a49327e3c48
bruce <1142634487@qq.com>
2019-03-07 18:12:23 +0800

    test

--- src/components/CheckList.vue
+++ src/components/CheckList.vue
@@ -73,11 +73,7 @@ export default {
     window.rightClick = this.changeShow
     window.leftClick = this.changeHidden
     tool.nativeEvent('setTitle', this.newTitle)
-    console.log('checklist')
-    console.log(this.leftClick)
-    console.log(window.leftClick)
-    console.log(this.changeHidden)
-    console.log(window.leftClick)
+    console.log(this.checkedArr)
   },
   beforeDestroy () {
     if (this.rightClick) {
