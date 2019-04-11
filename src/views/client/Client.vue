aec261be73109f60d9a505cef336e67a4371f15f
bruce <1142634487@qq.com>
2019-04-11 19:23:54 +0800

    fix

--- src/views/client/Client.vue
+++ src/views/client/Client.vue
@@ -9,7 +9,7 @@
     <Cell title="负责人" class="require" @click.native="checkKeyMan" isLink>
         <img class="avatar" v-for="(item, index) in principalIconArr" :src="item.icon_url" :key="index">
       </Cell>
-    <Field label="联系人" class="require" v-model="contactName"  @blur.native="return false" />
+    <Field label="联系人" class="require" v-model="contactName" ref="contactName"  @blur.native="reBackPos('contactName')" />
     <Cell title="关键决策人" class="require" @click.native="changeState('keyVisible', !keyVisible)" :value="isKeyName" isLink></Cell>
     <Selector :visible="keyVisible" :data="yesOrNoArr" @change="checkKey" />
     <Field label="联系人电话" class="require" v-model="contactPhone" />
@@ -344,6 +344,10 @@ export default {
         this.principalId = this.principalIconArr[0].id || ''
       })
     },
+    // 恢复原始位置
+    reBackPos (name) {
+      this.$refs[name].scrollIntoView(false)
+    }
   }
 }
 </script>
